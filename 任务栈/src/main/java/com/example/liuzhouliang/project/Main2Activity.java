76813ed989bae331af2ae9195eeff03f9844d476
liuzhouliang <liuzhouliang@gomeplus.com>
2017-07-07 10:35:05 +0800

    branch

--- 任务栈/src/main/java/com/example/liuzhouliang/project/Main2Activity.java
+++ 任务栈/src/main/java/com/example/liuzhouliang/project/Main2Activity.java
@@ -6,7 +6,7 @@ import android.widget.TextView;
 
 public class Main2Activity extends AppCompatActivity {
     /**
-     * new
+     * new====
      * @param savedInstanceState
      */
     @Override
